904c76c2d39a63f0911a7d7b06051bd8750bbf2f
cp-projects <cpeter52@depaul.edu>
2021-08-29 04:41:13 -0500

    most of the structure is done, need more work on making the transition function account for multiple chars as an input

--- fig1Pont2.cpp
+++ fig1Pont2.cpp
@@ -1,8 +1,90 @@
 #include <iostream>
+#include <map>
+#include <set>
+
+using namespace std;
+
+
+map<pair<char, char>, char> delta = {
+
+//start state
+{{'c', 'n'}, 'c'},
+
+//Trasistion states
+{{'c','f'}, 'o'},
+{{'o', 'n'}, 'c'},
+
+//stay closed states
+{{'c','r'}, 'c'},
+{{'c', 'b'}, 'c'},
+                                       
+
+//stay opened stated
+{{'o','f'}, 'o'},
+{{'o', 'r'}, 'o'},
+{{'o', 'b'}, 'o'},
+
+
+        
+};
+
+
+
+map<char, char> delta2 = {
+
+    {'h', 'a'},
+    {'e', 'b'},
+    {'l', 'c'},
+    {'l', 'd'},
+    {'o', 'e'},
+
+
+   /*	
+    {{'h','d'}, 'a'},
+    {{'e', 'd'}, 'b'},
+    {{'l', 'd'}, 'c'},
+    */
+
+};
+
+
+
+
+
+bool accept(string w){
+
+    char q;
+
+    for(char a: w){
+       //cout << a << ' ';
+       
+	cout << delta2[a] << ' ';
+
+        }
+
+    cout << '\n';
+
+ return true;
+
+}
+
+
+
+
 
 int main(){
 
-    std::cout << "hello World";
+    set<char> Q = {'o','c'};
+
+    set<char> sigma = {'f','r','b','n'};
+
+    set<char> F = {'o'};
+
+
+
+    accept("hello"); 
+    
+    
 
 
 
